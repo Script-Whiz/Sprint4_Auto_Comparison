06f67d60c0bde08cd3877413fd4dc7480ceb4d07
Deborah Thomas <debheydt@gmail.com>
2025-03-24 23:00:42 -0600

    Resolve merge conflicts

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 
 # Load the data from the CSV file
-autos = pd.read_csv('autos_data.csv')
+autos = pd.read_csv('datasets/autos_data.csv')
 
 # Create a header
 st.header("Comparison of Auto Sales from 1908-2019")
